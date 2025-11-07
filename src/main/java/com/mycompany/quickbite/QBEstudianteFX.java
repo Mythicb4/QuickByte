71227216f68f90f34c7f284f001a6ddb130fe8d7
Mythicb4 <emmanuelecheniqueramirez@gmail.com>
2025-11-07 18:56:42 -0500

    Vista y controlador de estudiante añadido (login_estudiante.fxml, QBEstudianteFX)

---
+++ src/main/java/com/mycompany/quickbite/QBEstudianteFX.java
@@ -0,0 +1,114 @@
+package com.mycompany.quickbite;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class QBEstudianteFX {
+
+    @FXML
+    private HBox HBCompraExpress;
+
+    @FXML
+    private HBox HBCompraExpress1;
+
+    @FXML
+    private StackPane SPProduct;
+
+    @FXML
+    private StackPane SPProduct1;
+
+    @FXML
+    private StackPane SPProduct2;
+
+    @FXML
+    private StackPane SPProduct3;
+
+    @FXML
+    private Button btnCarrito;
+
+    @FXML
+    private Button btnCompraExpress;
+
+    @FXML
+    private Button btnCompraExpress1;
+
+    @FXML
+    private Button btnCompraExpress2;
+
+    @FXML
+    private Button btnCompraExpress3;
+
+    @FXML
+    private Button btnHistorial;
+
+    @FXML
+    private Button btnMore;
+
+    @FXML
+    private Button btnPerfil;
+
+    @FXML
+    private Button btnPreferencias;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Button btnTienda;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private ImageView imgProduct;
+
+    @FXML
+    private ImageView imgProduct1;
+
+    @FXML
+    private ImageView imgProduct2;
+
+    @FXML
+    private ImageView imgProduct3;
+
+    @FXML
+    private Label lblCNames;
+
+    @FXML
+    private Label lblLogo;
+
+    @FXML
+    private Label lblPName;
+
+    @FXML
+    private Label lblPName1;
+
+    @FXML
+    private Label lblPName2;
+
+    @FXML
+    private Label lblPName3;
+
+    @FXML
+    private Label lblPPrice;
+
+    @FXML
+    private Label lblPPrice1;
+
+    @FXML
+    private Label lblPPrice2;
+
+    @FXML
+    private Label lblPPrice3;
+
+    @FXML
+    void onBtnSingup(ActionEvent event) {
+
+    }
+
+}
